7318211638f381394a3e89fcddeef91cde8d6a22
nanda asusvivobook <105841110622@student.unismuh.ac.id>
2024-07-31 21:39:44 +0800

    selesai chapter 2

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-screen p-6">
-      <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
-        {/* <AcmeLogo /> */}
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <AcmeLogo />
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
@@ -26,7 +26,12 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <img
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
+        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
     </main>
   );
