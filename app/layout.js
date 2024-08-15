3e3b72ae2d57c7e3f40a75ae97bc16907010e9c8
nandasulystrian <105841110622@student.unismuh.ac.id>
2024-08-15 09:50:03 +0800

    Selesai chapter 3

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import "@/app/ui/global.css";
+import { lusitana } from "./ui/fonts";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
